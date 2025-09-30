4ba9a46febdabf54ab812957e40ee36795eff212
ChoRdChario <rin9270@gmail.com>
2025-10-01 04:11:34 +0900

    1

--- app/drive_glb.js
+++ app/drive_glb.js
@@ -1,10 +1,1 @@
-export async function downloadGlbBlob(fileId){
-  if (!window.gapi?.client) throw new Error('gapi not ready');
-  const res = await gapi.client.drive.files.get({ fileId, alt: 'media' });
-  const blob = new Blob([res.body], { type: 'model/gltf-binary' });
-  return blob;
-}
-export async function getName(fileId){
-  const meta = await gapi.client.drive.files.get({ fileId, fields: 'name' });
-  return meta.result?.name || 'model.glb';
-}
+export async function downloadGlbBlob(fileId){if(!window.gapi?.client)throw new Error('gapi not ready');const res=await gapi.client.drive.files.get({fileId,alt:'media'});return new Blob([res.body],{type:'model/gltf-binary'});}export async function getName(fileId){const meta=await gapi.client.drive.files.get({fileId,fields:'name'});return meta.result?.name||'model.glb';}
