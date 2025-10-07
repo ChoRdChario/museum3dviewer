e7ddf105fe824458d0307601baffcdff174fc633
ChoRdChario <rin9270@gmail.com>
2025-10-08 00:22:23 +0900

    リセット

---
+++ gauth.js
@@ -0,0 +1,4 @@
+// gauth.js - classic-safe stub (2025-10-07)
+// This file intentionally does nothing. The real module is 'gauth.module.js'.
+// If you see this being executed, remove any <script src="./gauth.js"> tags from HTML.
+console && console.warn && console.warn('[gauth] classic stub loaded. Use app_boot.js (module) to import gauth.module.js.');
